7e6b0bb6932a42d509ce61e967fdf2028ef00817
Dhanushka <skydigitalweb01@gmail.com>
2018-06-24 17:09:21 +0530

    Initial commit

---
+++ class_and_object.php
@@ -0,0 +1,48 @@
+
+
+<?php
+
+class Dress{
+
+    public $color="Red";
+    public $fabric="Lined";
+    public $design="slim fit";
+}
+
+
+
+$dressobject=new Dress();
+
+echo "</br></br>";
+var_dump($dressobject);
+echo "</br></br>";
+print_r($dressobject);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
